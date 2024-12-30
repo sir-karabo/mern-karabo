ba57c960fc27d7e4254d2bd9e0849af3d824d276
sir-karabo <sir.nhlamulo@gmail.com>
2024-12-30 12:15:20 +0200

    set up server.js + auth routes

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express()
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+const authRoutes = require('./src/users/user.route.js')
+//
+
+// middleware 
+app.use(express.json({ limit: "25mb" }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+//routes 
+
+
+main()
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+
+    app.get('/', (req, res) => {
+        res.send('Sir Karabo E-commerce Server is running !')
+    })
+}
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+}) 
