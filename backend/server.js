973dec9c09e1438111b7a3debfd9c609d036981b
sir-karabo <sir.nhlamulo@gmail.com>
2024-12-30 21:34:45 +0200

    db connection

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,7 @@ require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
-const authRoutes = require('./src/users/user.route.js')
-const authModels = require('./src/users/user.models.js')
+const authRoutes = require('./src/users/user.route')
 //
 
 // middleware 
@@ -15,7 +14,6 @@ app.use(express.json({ limit: "25mb" }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
---
+++ backend/src/users/user.route.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+// const User = require('./user.model')
+// const generateToken = require('../middleware/generate-token')
+
+// Register endpoint
+router.get('/register', async (req, res) => {
+    try {
+        // const { username, email, password } = req.body
+        // const user = new User({ email, username, password })
+
+        // await user.save()
+        res.send({ message: 'User registered successfully!' })
+    } catch (error) {
+        console.error('Error registering user', error)
+        res.status(500).send({ message: 'Error registering user' })
+    }
+})
+
+module.exports = router
